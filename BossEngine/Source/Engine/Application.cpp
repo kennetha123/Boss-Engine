766f998add605223976f837057ec2d63032e418e
kennetha123 <jodysag5@gmail.com>
2020-11-18 20:46:02 +0700

    [Window] Integrate window events.

--- BossEngine/Source/Engine/Application.cpp
+++ BossEngine/Source/Engine/Application.cpp
@@ -1,16 +1,19 @@
 #include "bepch.h"
 #include "Application.h"
 
-#include "Events/ApplicationEvent.h"
 #include "Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace BossEngine
 {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	BossEngine::Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	BossEngine::Application::~Application()
@@ -18,17 +21,28 @@ namespace BossEngine
 
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		BE_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
-		while (true)
+		while (m_Running)
 		{
-			while (m_Running)
-			{
-				glClearColor(1, 0, 1, 1);
-				glClear(GL_COLOR_BUFFER_BIT);
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 
-				m_Window->OnUpdate();
-			}
+			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- BossEngine/Source/Engine/Application.h
+++ BossEngine/Source/Engine/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace BossEngine
@@ -12,10 +13,15 @@ namespace BossEngine
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
 
+	// define Engine in Client-side.
 	Application* CreateApplication();
 }
