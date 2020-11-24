b5403d17459237bc5eca6134a5ee7f919564c5d7
kennetha123 <jodysag5@gmail.com>
2020-11-25 04:51:36 +0700

    [Windows] Add ImGui Events

--- BossEngine/Source/Engine/Events/Event.h
+++ BossEngine/Source/Engine/Events/Event.h
@@ -24,6 +24,7 @@ namespace BossEngine
 		// key
 		KeyPressed,
 		KeyReleased,
+		KeyTyped,
 		
 		// mouse
 		MouseButtonPressed,
--- BossEngine/Source/Engine/ImGui/ImGuiLayer.h
+++ BossEngine/Source/Engine/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Engine/Layer.h"
 
+#include "Engine/Events/ApplicationEvent.h"
+#include "Engine/Events/KeyEvent.h"
+#include "Engine/Events/MouseEvent.h"
+
 namespace BossEngine
 {
 	class BE_API ImGuiLayer : public Layer
@@ -14,8 +18,16 @@ namespace BossEngine
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
-
 	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 		float m_Time = 0.0f;
 	};
 }
